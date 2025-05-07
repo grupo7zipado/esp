896040ef39c26395b235ab9fafdab84881d1fb79
Midiel <midielcastroramires400@gmail.com>
2025-05-06 22:14:27 -0300

    Bibliotecas
    descrição bibliotecas

---
+++ bkp/coment.ino
@@ -0,0 +1,33 @@
+/*
+    BIBLIOTECAS
+*/
+
+
+/*
+    VARIÁVEIS DE SETUP
+*/
+
+
+/*
+    FUNÇÕES DE SETUP
+*/
+
+
+/*
+    FUNÇÕES DE LEITURA
+*/
+
+
+/*
+    SISTEMA OPERACIONAL
+*/
+
+
+/*
+    VOID SETUP
+*/
+
+
+/*
+    VOID LOOP
+*/
--- coment.ino
+++ coment.ino
@@ -1,7 +1,18 @@
 /*
     BIBLIOTECAS
 */
-
+#include <Wire.h> //configuração de I2C
+#include <WiFi.h> //biblioteca WiFi
+#include <NTPClient.h> //data e hora
+#include <driver/adc.h> //nível de bateria
+#include "MAX30105.h" //sensor BPM/Oxigenação
+#include "heartRate.h" //processamento de BPM?Oxigenação
+#include <Adafruit_MLX90614.h> //sensor de temperatura
+#include <Adafruit_SSD1306.h> //controle do display
+#include <Adafruit_GFX.h> //biblioteca grafica do display
+#include <PubSubClient.h> //broker MQTT
+#include <ArduinoJson.h> // dados em formato Json
+#include <Preferences.h> //
 
 /*
     VARIÁVEIS DE SETUP
