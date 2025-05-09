b39335be41f3af379c7cc4127fcda496404565fb
gabriell122 <s0mbr4h@gmail.com>
2025-05-08 21:54:21 -0300

    up

---
+++ esp.ino
@@ -0,0 +1,33 @@
+/*
+    Bibliotecas
+*/
+
+
+/*
+    Variaveis de setup
+*/
+
+
+/*
+    Funções de setup
+*/
+
+
+/*
+    Funções de leituras
+*/
+
+
+/*
+    Sistema Operacional
+*/
+
+
+/*
+    Setup
+*/
+
+
+/*
+    Loop
+*/
